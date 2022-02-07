7a8c2465264fb0b4e166bd23b31e78e8dcc9cf04
Sourav Mitra <you@example.com>
2022-02-07 23:53:49 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/*let country = 'India';
+let continent = 'Asia';
+let population = '12 Million';
+
+ console.log(country+' '+continent);
+ */
+
+// Test Case 1 :
+
+let markWeight = 78;
+let johnWeight = 92;
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+let markBMI = markWeight/(markHeight ** 2);
+let johnBMI = johnWeight/(johnHeight ** 2);
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI,johnBMI,markHigherBMI);
+
+//Test Case 2
+
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+
+markBMI = markWeight/(markHeight ** 2);
+johnBMI = johnWeight/(johnHeight ** 2);
+
+markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI,johnBMI,markHigherBMI);
